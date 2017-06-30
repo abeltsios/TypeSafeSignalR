e8929d5cab6ed34ebfbfca34c958691c1d234086
larry <a.mpeltsios@gmail.com>
2017-06-30 20:35:55 +0300

    initial

---
+++ TypeSafeSignalR/ClientHubSubscriber.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Newtonsoft.Json.Linq;
+
+namespace StreamCentral.Common
+{
+    public static class ClientHubSubscriber<T> where T : class
+    {
+        public static void CreateSubs<K>(IHubProxy hubProxy, K imp) where K : T
+        {
+            foreach (var interfaceMethod in typeof(T).GetMethods(BindingFlags.Instance | BindingFlags.Public))
+            {
+                string eventName = interfaceMethod.Name;
+                var retTp = interfaceMethod.ReturnType;
+                long paramCount = interfaceMethod.GetParameters().Length;
+
+                Subscription subscription = hubProxy.Subscribe(eventName);
+
+                var paramInfo = interfaceMethod.GetParameters().ToList();
+
+                subscription.Received += delegate (IList<JToken> list)
+                {
+                    List<object> invocParams = new List<object>();
+                    int idx = 0;
+                    foreach (JToken jToken in list)
+                    {
+                        invocParams.Add(jToken.ToObject(paramInfo[idx].ParameterType));
+                        ++idx;
+                    }
+                    interfaceMethod.Invoke(imp, invocParams.ToArray());
+                };
+            }
+        }
+    }
+}
---
+++ TypeSafeSignalR/ServerHubProxyHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace StreamCentral.Common
+{
+    public class ServerHubProxyHandler<T> : RealProxy
+    {
+        private readonly IHubProxy _hubProxy;
+        //private readonly MethodInfo _hubInvokableMethod;
+        private ServerHubProxyHandler(IHubProxy hubProxy) : base(typeof(T))
+        {
+            this._hubProxy = hubProxy;
+        }
+
+        public static T Create(IHubProxy hubProxy)
+        {
+            return (T)new ServerHubProxyHandler<T>(hubProxy).GetTransparentProxy();
+        }
+
+        public override IMessage Invoke(IMessage msg)
+        {
+            var methodCall = (IMethodCallMessage)msg;
+            var method = (MethodInfo)methodCall.MethodBase;
+
+            try
+            {
+                Console.WriteLine(method.Name);
+                if (method.ReturnType != typeof(void))
+                {
+                    var t = method.ReturnType;//typeof(Task<>).MakeGenericType(new Type[] { method.ReturnType });
+                    //var task = //_hubProxy.Invoke<object>(method.Name, methodCall.InArgs);
+                    //task<T>
+
+                    var _hubInvokableMethod = typeof(IHubProxy).GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                            .Single(m => m.IsGenericMethod && m.ReturnType.IsGenericType && m.GetParameters().Count() == 2);
+
+                    var signalRInvoked = (_hubInvokableMethod.MakeGenericMethod(t).Invoke(_hubProxy, new object[] { method.Name, methodCall.InArgs }));
+                    //getting Task<T> result
+                    var res = signalRInvoked.GetType().GetProperty("Result").GetValue(signalRInvoked);
+                    return new ReturnMessage(res, null, 0, methodCall.LogicalCallContext, methodCall);
+                }
+                else
+                {
+                    var task = _hubProxy.Invoke(method.Name, methodCall.InArgs);
+                    task.Wait();
+                    return new ReturnMessage(null, null, 0, methodCall.LogicalCallContext, methodCall);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e);
+                if (e is TargetInvocationException && e.InnerException != null)
+                {
+                    return new ReturnMessage(e.InnerException, msg as IMethodCallMessage);
+                }
+
+                return new ReturnMessage(e, msg as IMethodCallMessage);
+            }
+        }
+    }
+}
